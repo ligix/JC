781750975892271348b28c969c113bb1c7b4afdd
Your Name <you@example.com>
2024-07-21 14:23:19 +0200

    Having fun trying to understand how SHT_RELA works

--- testing_elf_stuff/read_elf.c
+++ testing_elf_stuff/read_elf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <elf.h>
+#include <stdbool.h>
 
 char* get_n_string(char* start, int n) {
     int c = 0;
@@ -13,9 +14,10 @@ char* get_n_string(char* start, int n) {
 int main(void) {
     Elf64_Ehdr elf_header;
 
-    FILE* file = fopen("hello", "rb");
+    FILE* file = fopen("python3.12", "rb");
     if (file == NULL) {
         printf("fopen failed with errno: %d", errno);
+        return 1;
     }
 
     fread(&elf_header, sizeof(elf_header), 1, file);
@@ -33,10 +35,26 @@ int main(void) {
     fseek(file, shstrtab_header.sh_offset, SEEK_SET);
     fread(&shstrtab_content, sizeof(char), shstrtab_header.sh_size, file);
 
+
+    // I can see three main ways move the relocation metadata (needed because I need to make it bigger):
+    // 1) "Deleting" the previous section without any fixup
+    // 2) "Deleting" the previous section and fixing all references across sections
+    // 3) Leaving the unreferenced section where it was
+    //
+    // 1- May break programs when the relocation metadata is between sections which have references between them (think
+    //    .text and .bss with address relative to RIP)
+    // 2- Requires way too much work, I'd need to parse and fix instructions and even that is not guaranteed to work (packers
+    //    which unpack only code which then references the same .bss as the packer)
+    // 3- Makes the binary way bigger because not only are we adding a LOT of relocation metadata to achieve the desired
+    //    computation but we are also duplicating the old one
+    //
+    // I'm lazy so 3 it is
+    //
+    // 4) Create a new relocation section, duh
+
     for (int i=0; i<elf_header.e_shnum; i++) {
         Elf64_Shdr section_header = section_headers[i];
 
-        printf("SECTION: %s\n", &shstrtab_content[section_header.sh_name]);
         printf("\ttype: %u\n", section_header.sh_type);
         printf("\tflags: %lu\n", section_header.sh_flags);
         printf("\taddr: %lu\n", section_header.sh_addr);
@@ -47,4 +65,19 @@ int main(void) {
         printf("\taddralign: %lu\n", section_header.sh_addralign);
         printf("\tentsize: %lu\n\n", section_header.sh_entsize);
     }
+
+    Elf64_Shdr relocation_header;
+    bool found_relocation_header = false;
+    for (int i=0; i<elf_header.e_shnum; i++) {
+        relocation_header = section_headers[i];
+        if (relocation_header.sh_type == SHT_RELA){
+            found_relocation_header = true;
+            break;
+        }
+    }
+    if (!found_relocation_header) {
+        printf("Binary does not seem to have a SHT_RELA section");
+        return 1;
+    }
+
 }
